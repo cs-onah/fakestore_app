63d22733ff932ad7cbbda887339e55d145713422
Ameh Christopher <akrine2christophine@gmail.com>
2023-01-09 13:32:16 +0100

    set up share preference

---
+++ lib/utils/user_simple_preference.dart
@@ -0,0 +1,21 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserSimplePreference{
+  static late SharedPreferences _preferences;
+
+  static const _keyUsername = 'username';
+  static const _keyPassword = 'password';
+
+  static Future init() async => _preferences = await SharedPreferences.getInstance();
+
+  static Future setUsername(String username) async =>
+      await _preferences.setString(_keyUsername, username);
+
+  static  String getUsername() => _preferences.getString(_keyUsername) ?? "";
+
+  static Future setPassword(String password) async =>
+      await _preferences.setString(_keyPassword, password);
+
+  static String getPassword() => _preferences.getString(_keyPassword) ?? "";
+
+}
