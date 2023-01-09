2fa1bd958892a46e029bb1b0dc4a4c89b8322493
Ameh Christopher <akrine2christophine@gmail.com>
2023-01-09 13:23:04 +0100

    display product on the home screen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,15 +1,81 @@
+import 'package:ecommerce_app/presentation/screens/product_detail_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:ecommerce_app/models/product.dart';
+import 'package:ecommerce_app/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
+import 'cart_screen.dart';
+
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  ApiService get service => GetIt.I<ApiService>();
 
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        actions: [
+          IconButton(
+              onPressed: (){},
+              icon: const Icon(Icons.search, size: 30.0,)
+          ),
+          IconButton(
+            icon: const Icon(Icons.shopping_cart, size: 30.0,),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) =>  CartScreen(),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FutureBuilder<List<Product>>(
+          future: service.getAllProducts(),
+          builder: (_, AsyncSnapshot<List<Product>> snapshot){
+            if(!snapshot.hasData){
+              return const Center(child: CircularProgressIndicator());
+            }
+            final product = snapshot.data!;
+            return GridView.builder(
+                itemCount: product.length,
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 4
+                ),
+                itemBuilder: ((context, index){
+                  final product = snapshot.data![index];
+                  return Column(
+                    children: [
+                      const SizedBox(height: 20,),
+                      Image.network(
+                        product.image ?? '',
+                        height: 60,
+                        width: 60,
+                      ),
+                      ListTile(
+                        title: Text(product.title, textAlign: TextAlign.left, overflow: TextOverflow.ellipsis,),
+                        subtitle: Text("\$${product.price}",
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.black),
+                        ),
+                        onTap: (){
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (_)=>  ProductDetailScreen(id: product.id,))
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                }),
+             //   separatorBuilder: (_,__) => const SizedBox(height: 4,),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
